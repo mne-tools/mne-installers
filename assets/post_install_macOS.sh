d27ae9c6e3f5c8a9ce8f4807430b0d79ec34c48e
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-02-20 15:37:44 +0100

    More tests & comments

--- assets/post_install_macOS.sh
+++ assets/post_install_macOS.sh
@@ -8,6 +8,7 @@ chown -R $USER/Applications/MNE*.app $USER
 echo "Moving MNE .app bundles from ${USER}/Applications to /Applications"
 mv $USER/Applications/MNE*.app /Applications
 
+# https://conda-forge.org/docs/user/tipsandtricks.html#installing-apple-intel-packages-on-apple-silicon
 echo "Configuring conda to only use Intel packages -- even on Apple Silicon -- when invoked by the user"
 echo "{\"env_vars\": {\"CONDA_SUBDIR\": \"osx-64\"}}" >> "${DSTROOT}/mne-python_1.0.0_0/conda-meta/state"
 chown "${DSTROOT}/mne-python_1.0.0_0/conda-meta/state" $USER
