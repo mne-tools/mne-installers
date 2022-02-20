cdcd98df66e1981ad35df2cb29d454759ea57748
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-02-20 19:22:33 +0100

    Better logging

--- assets/post_install_macOS.sh
+++ assets/post_install_macOS.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script must be marked +x to work correctly with the installer!
 
-echo "Fixing permissions of MNE .app bundles: new owner will be ${USER}"
+logger -p 'install.info' "ℹ️ Running the custom MNE-Python post-install script."
+
+logger -p 'install.info' "ℹ️ Fixing permissions of MNE .app bundles: new owner will be ${USER}"
 chown -R $USER/Applications/MNE*.app $USER
 
-echo "Moving MNE .app bundles from ${USER}/Applications to /Applications"
+logger -p 'install.info' "ℹ️ Moving MNE .app bundles from ${USER}/Applications to /Applications"
 mv $USER/Applications/MNE*.app /Applications
 
 # https://conda-forge.org/docs/user/tipsandtricks.html#installing-apple-intel-packages-on-apple-silicon
-echo "Configuring conda to only use Intel packages -- even on Apple Silicon -- when invoked by the user"
-echo "{\"env_vars\": {\"CONDA_SUBDIR\": \"osx-64\"}}" >> "${DSTROOT}/mne-python_1.0.0_0/conda-meta/state"
+logger -p 'install.info' "ℹ️ Configuring conda to only use Intel packages -- even on Apple Silicon -- when invoked by the user"
+logger -p 'install.info' "{\"env_vars\": {\"CONDA_SUBDIR\": \"osx-64\"}}" >> "${DSTROOT}/mne-python_1.0.0_0/conda-meta/state"
 chown "${DSTROOT}/mne-python_1.0.0_0/conda-meta/state" $USER
