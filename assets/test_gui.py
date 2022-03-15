0b471494bc2e3f6777782ec98de1f11567f07eb9
Eric Larson <larson.eric.d@gmail.com>
2022-03-15 14:48:31 -0400

    TST: Test GUI frameworks

---
+++ assets/test_gui.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pyvista
+
+import mne
+
+this_path = Path(__file__).parent
+
+# Matplotlib
+fig, _ = plt.subplots()
+want = 'MacOSX' if sys.platform == 'darwin' else 'QTAgg'
+assert want in repr(fig.canvas), repr(fig.canvas)
+plt.close('all')
+matplotlib.use('QtAgg', force=True)
+fig, _ = plt.subplots()
+want = 'QTAgg'
+assert want in repr(fig.canvas), repr(fig.canvas)
+plt.close('all')
+
+# pyvistaqt
+fname = this_path / 'test.png'
+mne.viz.set_3d_backend('pyvista')
+fig = mne.viz.create_3d_figure((400, 400))
+plotter = fig.plotter
+plotter.add_orientation_widget(pyvista.Cube())  # Old test without color='b'
+plotter.add_mesh(pyvista.Cube())
+if fname.is_file():
+    os.remove(fname)
+assert not fname.is_file()
+plotter.screenshot(fname)
+assert fname.is_file()
+os.remove(fname)
+mne.viz.close_3d_figure(fig)
+assert '.BackgroundPlotter' in repr(plotter), repr(plotter)
+
+# pyqtgraph
+mne.viz.set_browser_backend('qt')
+raw = mne.io.RawArray(np.zeros((1, 1000)), mne.create_info(1, 1000., 'eeg'))
+fig = raw.plot()
+fig.close()
+assert 'PyQtGraphBrowser' in repr(fig), repr(fig)
