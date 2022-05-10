107905d57c468d71206c7e5ee293b5ef9ac4a201
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-05-10 22:06:46 +0200

    Include pqdm (#120)
    * Include pqdm
    
    Fixes #119
    
    * Use mne 1.0.2 build 2
    
    * Fix version check

--- tests/test_json_versions.py
+++ tests/test_json_versions.py
@@ -17,20 +17,46 @@ sys_name = dict(
 with open(dir_ / 'recipes' / 'mne-python_1.0' / 'construct.yaml',
           encoding='utf-8') as fid:
     params = yaml.safe_load(fid)
-want_version = params['version']
+installer_version = params['version']
+specs = params['specs']
+del params
 
-fname = dir_ / f'MNE-Python-{want_version}-{sys_name}.env.json'
+# Extract versions from construct.yaml
+mne_package_names = ('mne', 'mne-installer-menus')  # the most important ones!
+want_versions = {}
+
+for spec in specs:
+    if not ' ' in spec:  # only include those where we specify a version
+        continue
+    package_name, package_version_and_build = spec.split(' ')
+    if package_name in mne_package_names:
+        package_version = package_version_and_build.split('=')[1]
+        package_build = (package_version_and_build
+                         .split('=')[-1]
+                         .replace('*', '')
+                         .replace('_', ''))
+        want_versions[package_name] = {
+            'version': package_version,
+            'build_number': package_build,
+        }
+
+# Extract versions from created environment
+fname = dir_ / f'MNE-Python-{installer_version}-{sys_name}.env.json'
 assert fname.is_file(), (fname, os.listdir(os.getcwd()))
-with open(fname, encoding='utf-8') as fid:
-    env_json = json.load(fid)
+env_json = json.loads(fname.read_text(encoding='utf-8'))
 got_versions = dict()
 for package in env_json:
-    if package['name'] in ('mne', 'mne-installer-menus'):
-        v, b = package['version'], package['build_number']
-        got_versions[package['name']] = f'{v}_{b}'
+    if package['name'] in mne_package_names:
+        got_versions[package['name']] = {
+            'version': str(package['version']),
+            'build_number': str(package['build_number']),
+        }
 assert len(got_versions) == 2, got_versions
 
 # check versions
-for name, version in got_versions.items():
-    msg = f'{name}: got {repr(version)} != want {repr(want_version)}'
-    assert version == want_version, msg
+for package_name in mne_package_names:
+    got = got_versions[package_name]
+    want = want_versions[package_name]
+
+    msg = f'{package_name}: got {repr(got)} != want {repr(want)}'
+    assert got == want, msg
