bf26ff227ce9138aa61ac286d57729ac7d6da3c8
Eric Larson <larson.eric.d@gmail.com>
2022-05-24 15:27:53 -0400

    FIX: Only patch when needed, sign in CI only

--- tools/patch_constructor.sh
+++ tools/patch_constructor.sh
@@ -4,6 +4,11 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 source ${SCRIPT_DIR}/extract_version.sh
 if [[ $MACHINE == "macOS" ]]; then
     CONSTRUCTOR_DIR=${CONDA_PREFIX}/lib/python${PYSHORT}/site-packages/constructor
-    echo "Patching constructor ${CONSTRUCTOR_DIR}..."
-    patch -d ${CONSTRUCTOR_DIR} -p1 < ${SCRIPT_DIR}/../assets/constructor_macOS.patch
+    PATCHFILE=${SCRIPT_DIR}/../assets/constructor_macOS.patch
+    if ! patch -Rd ${CONSTRUCTOR_DIR} -p1 -s -f --dry-run < $PATCHFILE; then
+        echo "Patching constructor ${CONSTRUCTOR_DIR}..."
+        patch -Nd ${CONSTRUCTOR_DIR} -p1 < $PATCHFILE
+    else
+        echo "Constructor already patched: ${CONSTRUCTOR_DIR}..."
+    fi
 fi
