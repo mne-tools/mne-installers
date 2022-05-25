6b71a0ad24d2a46a7577c6bdbf027e09b0b60fc9
Eric Larson <larson.eric.d@gmail.com>
2022-05-25 14:55:43 -0400

    FIX: Remove cruft

--- tools/build_local.sh
+++ tools/build_local.sh
@@ -8,5 +8,5 @@ echo "Version:      ${MNE_INSTALLER_VERSION}"
 echo "Recipe:       ${RECIPE_DIR}"
 echo "OS:           ${MACHINE}"
 echo "Architecture: ${PYARCH}"
-${SCRIPT_DIR}/patch_constructor.sh
+#${SCRIPT_DIR}/patch_constructor.sh
 ${SCRIPT_DIR}/run_constructor.sh
