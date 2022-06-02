df1eb55b7a3a47a0e92293fba94983f9cc79f0fb
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-06-02 23:43:14 +0200

    Apply platform-dependent patches on macOS

--- tools/patch_constructor.sh
+++ tools/patch_constructor.sh
@@ -2,14 +2,22 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 source ${SCRIPT_DIR}/extract_version.sh
-# TODO: Once the forked conda constructor is available on mac-arm64 natively,
-# we should remove the second part of this conditional
-if [[ $MACHINE == "macOS" && $PYMACHINE != "arm64" ]]; then
+
+if [[ $MACHINE == "macOS" ]]; then
     CONSTRUCTOR_DIR=${CONDA_PREFIX}/lib/python${PYSHORT}/site-packages/constructor
-    PATCHFILE=${SCRIPT_DIR}/../assets/constructor_macOS.patch
-    if ! patch -Rd ${CONSTRUCTOR_DIR} -p1 -s -f --dry-run < $PATCHFILE; then
+    PATCHFILE_COMMON=${SCRIPT_DIR}/../assets/constructor_macOS_common.patch
+    PATCHFILE_I386=${SCRIPT_DIR}/../assets/constructor_macOS_i386.patch
+    PATCHFILE_ARM64=${SCRIPT_DIR}/../assets/constructor_macOS_arm64.patch
+
+    if ! patch -Rd ${CONSTRUCTOR_DIR} -p1 -s -f --dry-run < $PATCHFILE_COMMON; then
         echo "Patching constructor ${CONSTRUCTOR_DIR}..."
-        patch -Nd ${CONSTRUCTOR_DIR} -p1 < $PATCHFILE
+        patch -Nd ${CONSTRUCTOR_DIR} -p1 < $PATCHFILE_COMMON
+
+        if [[ $PYMACHINE != "arm64" ]]; then
+            patch -Nd ${CONSTRUCTOR_DIR} -p1 < $PATCHFILE_ARM64
+        else
+            patch -Nd ${CONSTRUCTOR_DIR} -p1 < $PATCHFILE_I386
+        fi
     else
         echo "Constructor already patched: ${CONSTRUCTOR_DIR}"
     fi
