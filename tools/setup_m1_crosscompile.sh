dab978b88ec9e8f7a723b74511c692a029fcce73
Eric Larson <larson.eric.d@gmail.com>
2022-06-03 05:05:14 -0400

    ENH: Build for M1 (#126)

---
+++ tools/setup_m1_crosscompile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ef
+
+if ! conda list conda-standalone | grep conda-standalone; then
+    echo "Installing standalone"
+    conda install -yc napari/label/bundle_tools conda-standalone
+fi
+export PLATFORM_ARG="--platform=osx-arm64"
+export EXE_ARG="--conda-exe=${CONDA_PREFIX}/standalone_conda/conda.exe"
