30a5f5b5a0b54ea414f3334528eb81aad19fec72
Eric Larson <larson.eric.d@gmail.com>
2022-06-08 12:03:46 -0400

    FIX: Better

--- tools/extract_version.sh
+++ tools/extract_version.sh
@@ -19,9 +19,20 @@ if [[ "$MACHINE" != "macOS" && "$MACHINE" != "Linux" && "$MACHINE" != "Windows" 
 fi
 export MACHINE=$MACHINE
 export PYMACHINE=$(python -c "import platform; print(platform.machine())")
-export MNE_ARTIFACT_NAME="MNE-Python-$MACHINE-$PYMACHINE"
-if [[ ${PYMACHINE} == 'x86_64' ]]; then
-    export MACOS_SUFFIX="Intel"
-else
-    export MACOS_SUFFIX="M1"
+ARTIFACT_SUFFIX=$PYMACHINE
+MACOS_SUFFIX="Intel"
+# This env var only really gets set on GitHub
+if [[ ${CROSSCOMPILE_ARCH} == 'arm64' ]]; then
+    MACOS_SUFFIX="M1"
+    ARTIFACT_SUFFIX="arm64"
 fi
+MNE_INSTALLER_NAME="MNE-Python-${MACHINE}"
+if [[ "$MACHINE" == "macOS" ]]; then
+    MNE_INSTALLER_NAME="${MNE_INSTALLER_NAME}_${MACOS_SUFFIX}.pkg"
+elif [[ "$MACHINE" == "Linux" ]]; then
+    MNE_INSTALLER_NAME="${MNE_INSTALLER_NAME}.sh"
+else
+    MNE_INSTALLER_NAME="${MNE_INSTALLER_NAME}.exe"
+fi
+export MNE_INSTALLER_NAME="${MNE_INSTALLER_NAME}"
+export MNE_ARTIFACT_NAME="MNE-Python-${MACHINE}-${ARTIFACT_SUFFIX}"
