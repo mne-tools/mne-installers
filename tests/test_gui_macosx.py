371b63bc05bf68a6247fba040ccd966339c2cd0a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-06-17 12:34:25 +0200

    Disable special-casing macOS and assume qtagg is the default on all platforms

--- tests/test_gui_macosx.py
+++ tests/test_gui_macosx.py
@@ -1,7 +1,9 @@
-import matplotlib.pyplot as plt
+# Currently not used, leave here for historical reasons
 
-# Matplotlib default (MacOSX)
-fig, _ = plt.subplots()
-want = ' NSView '
-assert want in repr(fig.canvas), repr(fig.canvas)
-plt.close('all')
+# import matplotlib.pyplot as plt
+
+# # Matplotlib default (MacOSX)
+# fig, _ = plt.subplots()
+# want = ' NSView '
+# assert want in repr(fig.canvas), repr(fig.canvas)
+# plt.close('all')
