740222a5ee9baf3ab9d97a44c9df3b1c4af49d03
Eric Larson <larson.eric.d@gmail.com>
2022-12-12 11:34:54 -0500

    ENH: Better tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,8 @@
-import logging
+print('Running imports')
+import faulthandler
 
-logging.info('Running imports')
+faulthandler.enable()
+
 import os
 import sys
 from pathlib import Path
@@ -15,17 +17,17 @@ import mne
 this_path = Path(__file__).parent
 
 # Matplotlib
-logging.info('Running matplotlib tests')
+print('Running matplotlib tests')
 fig, _ = plt.subplots()
 want = 'QTAgg'
 assert want in repr(fig.canvas), repr(fig.canvas)
 plt.close('all')
 
 # pyvistaqt
-logging.info('Running pyvistaqt tests')
+print('Running pyvistaqt tests')
 fname = this_path / 'test.png'
 mne.viz.set_3d_backend('pyvista')
-fig = mne.viz.create_3d_figure((400, 400), scene=False)
+fig = mne.viz.create_3d_figure((400, 400), scene=False, show=True)
 plotter = fig.figure.plotter
 plotter.add_orientation_widget(pyvista.Cube())  # Old test without color='b'
 plotter.add_mesh(pyvista.Cube())
@@ -40,7 +42,7 @@ mne.viz.close_3d_figure(fig)
 assert '.BackgroundPlotter' in repr(plotter), repr(plotter)
 
 # mne-qt-browser
-logging.info('Running mne-qt-browser tests')
+print('Running mne-qt-browser tests')
 mne.viz.set_browser_backend('qt')
 raw = mne.io.RawArray(np.zeros((1, 1000)), mne.create_info(1, 1000., 'eeg'))
 fig = raw.plot()
@@ -48,7 +50,7 @@ fig.close()
 assert 'MNEQtBrowser' in repr(fig), repr(fig)
 
 # mne-kit-gui
-logging.info('Running mne-kit-gui tests')
+print('Running mne-kit-gui tests')
 from pyface.api import GUI  # noqa
 import mne_kit_gui  # noqa
 os.environ['_MNE_GUI_TESTING_MODE'] = 'true'
