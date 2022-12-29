1f9e1b82c6ee7033b5b68c55ba9651d136694144
Eric Larson <larson.eric.d@gmail.com>
2022-12-29 14:06:43 -0500

    FIX: Reverts

--- assets/post_install_windows.bat
+++ assets/post_install_windows.bat
@@ -1,9 +1,6 @@
 echo ℹConfiguring Python to ignore user-installed local packages.
 "%PREFIX%\bin\conda" env config vars set PYTHONNOUSERSITE=1
 
-echo ℹConfiguring Matplotlib to use the Qt5 backend by default.
-"%PREFIX%\bin\conda" env config vars set MPLBACKEND=qt5agg
-
 echo ℹDisabling mamba package manager banner.
 "%PREFIX%\bin\conda" env config vars set MAMBA_NO_BANNER=1
 
