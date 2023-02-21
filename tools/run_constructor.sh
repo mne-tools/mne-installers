b77c4ad88d880048725fcf87f2b54165c4a9ba36
Eric Larson <larson.eric.d@gmail.com>
2023-02-21 09:26:06 -0500

    MAINT: Update deps

--- tools/run_constructor.sh
+++ tools/run_constructor.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 source ${SCRIPT_DIR}/extract_version.sh
 export PYTHONUTF8=1
 # enforce UTF-8 encoding when reading files even on Windows
-echo "Running constructor recipe ${RECIPE_DIR}"
+echo "Running constructor recipe ${RECIPE_DIR} in verbose mode"
 # PLATFORM_ARG and EXE_ARG can be used for building macOS ARM64 on Intel
-constructor ${PLATFORM_ARG} ${EXE_ARG} ${RECIPE_DIR}
+constructor -v ${PLATFORM_ARG} ${EXE_ARG} ${RECIPE_DIR}
