38f51486685332ba0b13a81cee1507060a522db5
Eric Larson <larson.eric.d@gmail.com>
2023-07-13 15:51:52 -0400

    FIX: Wordpad

--- tools/run_constructor.sh
+++ tools/run_constructor.sh
@@ -6,4 +6,4 @@ export PYTHONUTF8=1
 # enforce UTF-8 encoding when reading files even on Windows
 echo "Running constructor recipe ${RECIPE_DIR} in verbose mode"
 # PLATFORM_ARG and EXE_ARG can be used for building macOS ARM64 on Intel
-constructor -vvv --debug ${PLATFORM_ARG} ${EXE_ARG} ${RECIPE_DIR}
+constructor -v ${PLATFORM_ARG} ${EXE_ARG} ${RECIPE_DIR}
