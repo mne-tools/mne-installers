467f449615f85d491a3953c7752fb25414d69cd3
Eric Larson <larson.eric.d@gmail.com>
2023-08-15 13:31:29 -0400

    FIX: condaaaaa

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -2,27 +2,35 @@ import platform
 
 
 def check_min_version(package, min_version):
+    """Check a minimum version."""
     from packaging.version import parse
-    assert parse(package.__version__) >= parse(min_version), f'{package}: got {package.__version__} wanted {min_version}'
+
+    assert parse(package.__version__) >= parse(
+        min_version
+    ), f"{package}: got {package.__version__} wanted {min_version}"
 
 
 import mne
-check_min_version(mne, '1.4')
+
+check_min_version(mne, "1.4")
 import mne_bids
 import mne_bids_pipeline
-check_min_version(mne_bids_pipeline, '1.2')
+
+check_min_version(mne_bids_pipeline, "1.2")
 import mne_connectivity
 import mne_faster
 import mne_nirs
 import mne_qt_browser
-check_min_version(mne_qt_browser, '0.5.0')
+
+check_min_version(mne_qt_browser, "0.5.0")
 import mne_realtime
 import mne_features
 import mne_rsa
 import mne_microstates
 import mne_ari
 import mne_kit_gui
-if platform.system() != 'Windows':
+
+if platform.system() != "Windows":
     import mne_icalabel
 import autoreject
 import pyprep
--- tests/test_json_versions.py
+++ tests/test_json_versions.py
@@ -9,52 +9,48 @@ import yaml
 dir_ = pathlib.Path(__file__).parent.parent
 
 sys_name = dict(
-    linux='Linux',
-    darwin='macOS_Intel',
-    win32='Windows',
+    linux="Linux",
+    darwin="macOS_Intel",
+    win32="Windows",
 )[sys.platform]
-sys_ext = dict(
-    linux='.sh',
-    darwin='.pkg',
-    win32='.exe'
-)[sys.platform]
-ver = (dir_ / 'assets' / 'current_version.txt').read_text().strip()
-with open(dir_ / 'recipes' / f'mne-python_{ver}' / 'construct.yaml',
-          encoding='utf-8') as fid:
+sys_ext = dict(linux=".sh", darwin=".pkg", win32=".exe")[sys.platform]
+ver = (dir_ / "assets" / "current_version.txt").read_text().strip()
+with open(
+    dir_ / "recipes" / f"mne-python_{ver}" / "construct.yaml", encoding="utf-8"
+) as fid:
     params = yaml.safe_load(fid)
-installer_version = params['version']
-specs = params['specs']
+installer_version = params["version"]
+specs = params["specs"]
 del params
 
 # Extract versions from construct.yaml
-mne_package_names = ('mne', 'mne-installer-menus')  # the most important ones!
+mne_package_names = ("mne", "mne-installer-menus")  # the most important ones!
 want_versions = {}
 
 for spec in specs:
-    if not ' ' in spec:  # only include those where we specify a version
+    if " " not in spec:  # only include those where we specify a version
         continue
-    package_name, package_version_and_build = spec.split(' ')
+    package_name, package_version_and_build = spec.split(" ")
     if package_name in mne_package_names:
-        package_version = package_version_and_build.split('=')[1]
-        package_build = (package_version_and_build
-                         .split('=')[-1]
-                         .replace('*', '')
-                         .replace('_', ''))
+        package_version = package_version_and_build.split("=")[1]
+        package_build = (
+            package_version_and_build.split("=")[-1].replace("*", "").replace("_", "")
+        )
         want_versions[package_name] = {
-            'version': package_version,
-            'build_number': package_build,
+            "version": package_version,
+            "build_number": package_build,
         }
 
 # Extract versions from created environment
-fname = dir_ / f'MNE-Python-{installer_version}-{sys_name}{sys_ext}.env.json'
+fname = dir_ / f"MNE-Python-{installer_version}-{sys_name}{sys_ext}.env.json"
 assert fname.is_file(), (fname, os.listdir(os.getcwd()))
-env_json = json.loads(fname.read_text(encoding='utf-8'))
+env_json = json.loads(fname.read_text(encoding="utf-8"))
 got_versions = dict()
 for package in env_json:
-    if package['name'] in mne_package_names:
-        got_versions[package['name']] = {
-            'version': str(package['version']),
-            'build_number': str(package['build_number']),
+    if package["name"] in mne_package_names:
+        got_versions[package["name"]] = {
+            "version": str(package["version"]),
+            "build_number": str(package["build_number"]),
         }
 assert len(got_versions) == 2, got_versions
 
@@ -63,5 +59,5 @@ for package_name in mne_package_names:
     got = got_versions[package_name]
     want = want_versions[package_name]
 
-    msg = f'{package_name}: got {repr(got)} != want {repr(want)}'
+    msg = f"{package_name}: got {repr(got)} != want {repr(want)}"
     assert got == want, msg
