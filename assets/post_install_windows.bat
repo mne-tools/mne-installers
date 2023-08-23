1825c917e5bc732bdf68a73560b96e2ea0bb6cf6
Eric Larson <larson.eric.d@gmail.com>
2023-08-23 14:58:21 -0400

    Pin BLAS implementation to OpenBLAS (#201)
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- assets/post_install_windows.bat
+++ assets/post_install_windows.bat
@@ -4,5 +4,8 @@ echo ℹConfiguring Python to ignore user-installed local packages.
 echo ℹDisabling mamba package manager banner.
 "%PREFIX%\bin\conda" env config vars set MAMBA_NO_BANNER=1
 
+echo ℹPinning BLAS implementation to OpenBLAS
+echo "libblas=*=*openblas" >>"%PREFIX%\bin\conda\conda-meta\pinned"
+
 echo ℹRunning mne sys_info.
 "%PREFIX%\bin\conda" run mne sys_info
