1cbe069e203bbcb087da73afc9d44f92f03d113b
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-11-21 20:32:56 +0100

    Which openssl

--- tools/macos_install_certificates.sh
+++ tools/macos_install_certificates.sh
@@ -39,10 +39,11 @@ sudo security add-trusted-cert -d -r trustRoot -k $KEYCHAIN_PATH ./AppleComputer
 security add-certificates -k $KEYCHAIN_PATH ./AppleWWDRCA.cer
 echo "✅ Done installing Apple certificates."
 
-# # ensure we're going to import the correct developer certificates into keychain
-# echo "🏃 Running sanity check on our Developer certificates before importing …"
-# echo "⛔️ WARNING: USING OPENSSL LEGACY MODE. PLEASE FIX THIS."
-# echo "Using OpenSSL:" `openssl version`
+# ensure we're going to import the correct developer certificates into keychain
+echo "🏃 Running sanity check on our Developer certificates before importing …"
+echo "⛔️ WARNING: USING OPENSSL LEGACY MODE. PLEASE FIX THIS."
+echo "Using OpenSSL:" `openssl version`
+echo `which openssl`
 # openssl pkcs12 -legacy -nokeys -passin pass:"$APPLICATION_CERT_PASSWORD" -in $APPLICATION_CERT_PATH | grep friendlyName
 # openssl pkcs12 -legacy -nokeys -passin pass:"$INSTALLER_CERT_PASSWORD" -in $INSTALLER_CERT_PATH | grep friendlyName
 # echo "✅ Done running sanity check on our Developer certificates before importing."
