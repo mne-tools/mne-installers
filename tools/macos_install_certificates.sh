c78135fc1001e8816635cd2655b0a80bddc3176c
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-11-21 20:34:51 +0100

    Use pre-installed OpenSSL binary

--- tools/macos_install_certificates.sh
+++ tools/macos_install_certificates.sh
@@ -42,11 +42,10 @@ echo "✅ Done installing Apple certificates."
 # ensure we're going to import the correct developer certificates into keychain
 echo "🏃 Running sanity check on our Developer certificates before importing …"
 echo "⛔️ WARNING: USING OPENSSL LEGACY MODE. PLEASE FIX THIS."
-echo "Using OpenSSL:" `openssl version`
-echo `which openssl`
-# openssl pkcs12 -legacy -nokeys -passin pass:"$APPLICATION_CERT_PASSWORD" -in $APPLICATION_CERT_PATH | grep friendlyName
-# openssl pkcs12 -legacy -nokeys -passin pass:"$INSTALLER_CERT_PASSWORD" -in $INSTALLER_CERT_PATH | grep friendlyName
-# echo "✅ Done running sanity check on our Developer certificates before importing."
+echo "Using OpenSSL:" `/usr/bin/openssl version`
+/usr/bin/openssl pkcs12 -nokeys -passin pass:"$APPLICATION_CERT_PASSWORD" -in $APPLICATION_CERT_PATH | grep friendlyName
+/usr/bin/openssl pkcs12 -nokeys -passin pass:"$INSTALLER_CERT_PASSWORD" -in $INSTALLER_CERT_PATH | grep friendlyName
+echo "✅ Done running sanity check on our Developer certificates before importing."
 
 # import developer certificates
 echo "🏃 Importing our Developer certificates …"
