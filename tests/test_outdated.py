6707c11e294b9ff3a9caa818bab820808aabdcde
Eric Larson <larson.eric.d@gmail.com>
2024-01-02 12:13:59 -0500

    FIX: Revert

--- tests/test_outdated.py
+++ tests/test_outdated.py
@@ -26,7 +26,8 @@ class Package:  # noqa: D101
 allowed_outdated: set[str] = {
     "python",  # ignore 3.12.0rc3
     "conda",
-    "mamba",  # conda and mamba conflict with tensorflow and VTK
+    "conda-libmamba-solver",
+    "mamba",  # conda/mamba conflict with tensorflow and VTK
     "tensorflow",  # 2.13.1 conflicts with VTK
     "graphviz",  # conflicts with VTK
     "pydata-sphinx-theme",  # haven't updated to latest version in our conf.py
