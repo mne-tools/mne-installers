fd83fbf7cd5a6b42839de8b80a8d14a4a7a0748d
Eric Larson <larson.eric.d@gmail.com>
2024-01-18 16:54:18 -0500

    MAINT: Remove bundle_tools_3 (#244)

--- tools/macos_check_installer_signature.sh
+++ tools/macos_check_installer_signature.sh
@@ -5,14 +5,14 @@ set -eo pipefail
 if [[ "$GITHUB_EVENT_NAME" != "pull_request" ]]; then
     pkgutil --check-signature ${MNE_INSTALLER_NAME} || exit 1
 fi
-# Now extract the package and check that the conda.exe binary is
+# Now extract the package and check that the _conda binary is
 # properly signed as well
 pkgutil --expand-full ${MNE_INSTALLER_NAME} ./mne-extracted
 DIR="./mne-extracted/prepare_installation.pkg/Payload/.mne-python"
 echo "Checking ${DIR} exists"
 test -d "$DIR"
 ls -al "$DIR"
-BINARY="${DIR}/conda.exe"
+BINARY="${DIR}/_conda"
 echo "Checking ${BINARY} exists"
 test -e "${BINARY}"
 echo "Checking ${BINARY} is signed"
