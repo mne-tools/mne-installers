ed4af1603398a27a2f9d34285f51cc324a6a5c36
Eric Larson <larson.eric.d@gmail.com>
2024-02-07 13:53:58 -0500

    FIX: Revert

--- tests/test_dev_installed.py
+++ tests/test_dev_installed.py
@@ -29,7 +29,8 @@ deps = [
     and not dep.startswith("PyQt6")
     # and not on conda-forge yet
     and not dep.startswith("sphinxcontrib-towncrier")
-    and not dep == "pytest"  # latest dev requires pytest >= 8.0 but stable uses < 8
+    and not dep == "pytest-8.0.0"  # dev requires pytest >= 8.0 but stable uses < 8
+    and not dep == "pyarrow-15.0.0"  # not tensorflow compatible
     # for some reason vtk is not detected properly on Windows even though it imports
     and not (platform.system() == "Windows" and dep.startswith("vtk"))
 ]
