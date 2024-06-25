1fd5050287bd529860e1d8b0cdda2d86ab62c132
Eric Larson <larson.eric.d@gmail.com>
2024-06-25 09:29:08 -0400

    FIX: Restore

--- tests/test_outdated.py
+++ tests/test_outdated.py
@@ -45,6 +45,7 @@ allowed_outdated: set[str] = {
     "mne-rsa",  # 0.91 appeared on conda-forge and was yanked
     "pyobjc-core",  # 10.3 conflicted with pyobjc-framework-cocoa on 2024/05/28
     "vtk",  # 9.3.0 is out but mayavi (at least) hasn't been migrated
+    "numba",  # conflicts with VTK 9.2.6
 }
 packages: list[Package] = []
 
