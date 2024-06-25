986db951f291f39869631380e9d90cd74b040615
Eric Larson <larson.eric.d@gmail.com>
2024-06-25 12:01:58 -0400

    MAINT: Bump pyobjc group (#275)

--- tests/test_outdated.py
+++ tests/test_outdated.py
@@ -43,7 +43,6 @@ allowed_outdated: set[str] = {
     "sphinx",  # 7.3 compat in progress
     "graphviz",  # conflicts with VTK 9.2.6 via libexpat
     "mne-rsa",  # 0.91 appeared on conda-forge and was yanked
-    "pyobjc-core",  # 10.3 conflicted with pyobjc-framework-cocoa on 2024/05/28
     "vtk",  # 9.3.0 is out but mayavi (at least) hasn't been migrated
     "numba",  # conflicts with VTK 9.2.6
     # https://github.com/raphaelvallat/yasa/pull/171
