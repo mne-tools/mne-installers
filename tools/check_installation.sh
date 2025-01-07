98fba521fe78231dcff224250e7339e2dd6ceb8a
Eric Larson <larson.eric.d@gmail.com>
2025-01-07 08:28:01 -0500

    FIX: Vers

--- tools/check_installation.sh
+++ tools/check_installation.sh
@@ -75,12 +75,12 @@ echo "Got OWNER=$OWNER, should be $(whoami)"
 test "$OWNER" == "$(whoami)"
 echo "::endgroup::"
 
-echo "::group:Checking whether Qt is working"
+echo "::group::Checking whether Qt is working"
 # LD_DEBUG=libs
 python -c "from qtpy.QtWidgets import QApplication, QWidget; app = QApplication([])"
 echo "::endgroup::"
 
-echo "::group:Checking the deployed environment variables were set correctly upon environment activation"
+echo "::group::Checking the deployed environment variables were set correctly upon environment activation"
 conda env config vars list
 if [[ "$MNE_MACHINE" == "macOS" && "$MACOS_ARCH" == "Intel" ]]; then
     python -c "import os; x = os.getenv('CONDA_SUBDIR'); assert x == 'osx-64', f'CONDA_SUBDIR ({repr(x)}) != osx-64'" || exit 1
@@ -96,13 +96,13 @@ echo "::group::mne sys_info"
 mne sys_info
 echo "::endgroup::"
 
-echo "::group:Trying to import MNE and all additional packages included in the installer"
+echo "::group::Trying to import MNE and all additional packages included in the installer"
 python -u tests/test_imports.py
 python -u tests/test_gui.py
 python -u tests/test_notebook.py
 python -u tests/test_json_versions.py
 echo "::endgroup::"
 
-echo "::group:Checking that all packages are installed that MNE-Python devs would need"
+echo "::group::Checking that all packages are installed that MNE-Python devs would need"
 python -u tests/test_dev_installed.py
 echo "::endgroup::"
