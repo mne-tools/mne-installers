2fa6c96f4ff3f3e2e48d7ad58ed03a046ce0b8d5
Eric Larson <larson.eric.d@gmail.com>
2025-02-10 13:27:15 -0500

    FIX: Echo

--- tests/test_spyder.sh
+++ tests/test_spyder.sh
@@ -3,7 +3,8 @@
 set -o pipefail
 
 TO=20s
-echo "Running Spyder with a timeout of $TO"
+echo "Running Spyder with a timeout of $TO:"
+echo "which spyder: $(which spyder)"
 timeout $TO spyder
 RESULT=$?
 if [[ $RESULT -eq 124 ]]; then
