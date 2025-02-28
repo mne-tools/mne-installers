23ab4d214bac413ad10478815eb98b2c65b80bbc
Eric Larson <larson.eric.d@gmail.com>
2025-02-28 09:35:21 -0500

    TST: Bisect

--- assets/post_install_windows.bat
+++ assets/post_install_windows.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 
-echo Setting read/write permissions on the Python installation to %USERNAME% (this can take a few minutes!).
-icacls %PREFIX% /grant %USERNAME%:(OI)(CI)M /c /t /q
+:: echo Setting read/write permissions on the Python installation to %USERNAME% (this can take a few minutes!).
+:: icacls %PREFIX% /grant %USERNAME%:(OI)(CI)M /c /t /q
 
-echo Configuring Python to ignore user-installed local packages.
-"%PREFIX%\Scripts\conda" env config vars set PYTHONNOUSERSITE=1
+:: echo Configuring Python to ignore user-installed local packages.
+:: "%PREFIX%\Scripts\conda" env config vars set PYTHONNOUSERSITE=1
 
-echo Disabling mamba package manager banner.
-"%PREFIX%\Scripts\conda" env config vars set MAMBA_NO_BANNER=1
+:: echo Disabling mamba package manager banner.
+:: "%PREFIX%\Scripts\conda" env config vars set MAMBA_NO_BANNER=1
 
-echo Pinning BLAS implementation to OpenBLAS.
-echo libblas=*=*openblas >> "%PREFIX%\conda-meta\pinned"
+:: echo Pinning BLAS implementation to OpenBLAS.
+:: echo libblas=*=*openblas >> "%PREFIX%\conda-meta\pinned"
 
-echo Running mne sys_info.
-"%PREFIX%\Scripts\conda" run mne sys_info
+:: echo Running mne sys_info.
+:: "%PREFIX%\Scripts\conda" run mne sys_info
