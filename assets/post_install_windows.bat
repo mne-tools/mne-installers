f0f57730a0707bd195dbd8157df9dd92249c0a2d
Eric Larson <larson.eric.d@gmail.com>
2025-02-28 10:37:18 -0500

    conda env config

--- assets/post_install_windows.bat
+++ assets/post_install_windows.bat
@@ -3,14 +3,14 @@
 echo Setting read/write permissions on the Python installation to %USERNAME% (this can take a few minutes!).
 icacls %PREFIX% /grant %USERNAME%:(OI)(CI)M /c /t /q
 
-:: echo Configuring Python to ignore user-installed local packages.
-:: "%PREFIX%\Scripts\conda" env config vars set PYTHONNOUSERSITE=1
+echo Configuring Python to ignore user-installed local packages.
+"%PREFIX%\Scripts\conda" env config vars set PYTHONNOUSERSITE=1
 
-:: echo Disabling mamba package manager banner.
-:: "%PREFIX%\Scripts\conda" env config vars set MAMBA_NO_BANNER=1
+echo Disabling mamba package manager banner.
+"%PREFIX%\Scripts\conda" env config vars set MAMBA_NO_BANNER=1
 
 :: echo Pinning BLAS implementation to OpenBLAS.
-:: echo libblas=*=*openblas >> "%PREFIX%\conda-meta\pinned"
+:: echo "libblas=*=*openblas" >> "%PREFIX%\conda-meta\pinned"
 
 :: echo Running mne sys_info.
 :: "%PREFIX%\Scripts\conda" run mne sys_info
