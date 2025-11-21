afe3a0c27d1d4d06f7c2c48ca9d5c5cba25523a7
Eric Larson <larson.eric.d@gmail.com>
2025-11-21 16:30:56 -0500

    FIX: What

--- tests/test_dev_installed.py
+++ tests/test_dev_installed.py
@@ -1,18 +1,35 @@
 import platform
 import re
 import subprocess
+from pathlib import Path
 
 would = "Would install "
+if not Path("../mne-python").is_dir():
+    print("Cloning mne-python repo...")
+    subprocess.check_output(
+        [
+            "git",
+            "clone",
+            "https://github.com/mne-tools/mne-python.git",
+        ],
+        cwd="..",
+    )
+print("Running pip install dry-run to check for unmet dependencies...")
 out = subprocess.check_output(
     [
         "pip",
         "install",
         "--no-build-isolation",
         "--dry-run",
-        # This can be used to speed up local testing
-        # "../mne-python[full,test,test_extra,doc]",
-        "mne[full-no-qt,test,test_extra,doc] @ git+https://github.com/mne-tools/mne-python.git@main",
-    ]
+        ".[full-no-qt]",
+        "--group",
+        "test",
+        "--group",
+        "test_extra",
+        "--group",
+        "doc",
+    ],
+    cwd="../mne-python",
 )
 out = out.decode("utf-8")
 deps = [line for line in out.split("\n") if line.startswith(would)]
@@ -25,21 +42,18 @@ deps = [
     dep
     for dep in deps.split()
     if not re.match("mne-[0-9]+", dep)
-    # TODO because we don't install neo
-    and not dep.startswith("neo-")
     and not dep.startswith("numpy-")
     and not dep.startswith("pyxdf-")
     and not dep.startswith("quantities-")
     # Qt-related stuff
     and not dep.startswith("sip-")
+    and not dep.startswith("tinycss2")
     # and not on conda-forge yet
     and not dep.startswith("sphinxcontrib-towncrier")
-    and not dep.startswith("antio")
     and not dep.startswith("toml-sort")
     and not dep.startswith("tomlkit")
-    and not dep == "pyarrow-15.0.0"  # not tensorflow compatible
     # for some reason vtk is not detected properly on Windows even though it imports
     and not (platform.system() == "Windows" and dep.startswith("vtk"))
 ]
 deps = "\n".join(deps)
-assert deps == "", f"Unexpected unmet dependencies:\n{deps}"
+assert deps == "", f"Unexpected unmet dependencies:\n{deps}\n\nOutput:\n{out}"
