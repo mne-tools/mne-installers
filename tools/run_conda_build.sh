45f6e383e6ba765c6be720f49178cbfc76ecc8af
Eric Larson <larson.eric.d@gmail.com>
2026-01-31 10:15:20 -0500

    Prevent installation of qt5

--- tools/run_conda_build.sh
+++ tools/run_conda_build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -ef
 
 echo "::group::Running conda-build recipe mne-installer-menu"
+set -x
 conda-build mne-installer-menu --no-anaconda-upload --croot conda-bld
+set +x
+echo "::endgroup::"
+
+echo "::group::Running conda-build recipe noqt5"
+set -x
+conda-build noqt5 --no-anaconda-upload --croot conda-bld
+set +x
 echo "::endgroup::"
