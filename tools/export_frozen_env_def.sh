8b42499c36cf78f6d1285868303e36a95b1ca111
Eric Larson <larson.eric.d@gmail.com>
2026-04-08 10:22:26 -0400

    FIX: DRY

--- tools/export_frozen_env_def.sh
+++ tools/export_frozen_env_def.sh
@@ -3,14 +3,13 @@
 set -eo pipefail
 
 source "${MNE_ACTIVATE}"
-MNE_JSON="${MNE_INSTALLER_NAME}.env.json"
-echo "Exporting frozen environment definition to ${MNE_JSON}"
-echo "::group::Exporting frozen environment definition to ${MNE_JSON}"
-conda list --json | tee ${MNE_JSON}
+echo "Exporting frozen environment definition to ${MNE_INSTALLER_JSON_NAME}"
+echo "::group::Exporting frozen environment definition to ${MNE_INSTALLER_JSON_NAME}"
+conda list --json | tee ${MNE_INSTALLER_JSON_NAME}
 echo "::endgroup::"
 
 echo "::group::Checking file size:"
-ACTUAL=$(du -k "${MNE_JSON}" | cut -f1)
+ACTUAL=$(du -k "${MNE_INSTALLER_JSON_NAME}" | cut -f1)
 echo "Actual size:  ${ACTUAL} KB"
 echo "Minimum size: 200 KB"
 test "$ACTUAL" -gt 200
