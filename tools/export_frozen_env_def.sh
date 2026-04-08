576de0b8d25fd5ea34150d1f3b3f320fa3daec3d
Eric Larson <larson.eric.d@gmail.com>
2026-04-08 09:58:47 -0400

    FIX: Streamline

--- tools/export_frozen_env_def.sh
+++ tools/export_frozen_env_def.sh
@@ -3,5 +3,15 @@
 set -eo pipefail
 
 source "${MNE_ACTIVATE}"
-conda list --json > ${MNE_INSTALLER_NAME}.env.json
-cat ${MNE_INSTALLER_NAME}.env.json
+MNE_JSON="${MNE_INSTALLER_NAME}.env.json"
+echo "Exporting frozen environment definition to ${MNE_JSON}"
+echo "::group::Exporting frozen environment definition to ${MNE_JSON}"
+conda list --json | tee ${MNE_JSON}
+echo "::endgroup::"
+
+echo "::group::Checking file size:"
+ACTUAL=$(du -k "${MNE_JSON}" | cut -f1)
+echo "Actual size:  ${ACTUAL} KB"
+echo "Minimum size: 200 KB"
+test "$ACTUAL" -gt 200
+echo "::endgroup::"
