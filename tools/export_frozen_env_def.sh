e47491a99478cfd30458b427c1159835feaf6c5c
Eric Larson <larson.eric.d@gmail.com>
2026-04-08 13:05:34 -0400

    FIX: Fine

--- tools/export_frozen_env_def.sh
+++ tools/export_frozen_env_def.sh
@@ -7,7 +7,7 @@ echo "::group::Exporting frozen environment definition to ${MNE_INSTALLER_JSON_N
 conda list --json | tee ${MNE_INSTALLER_JSON_NAME}
 echo "::endgroup::"
 
-echo "::group::Checking file size:"
+echo "::group::Checking file size"
 ACTUAL=$(du -k "${MNE_INSTALLER_JSON_NAME}" | cut -f1)
 echo "Actual size:  ${ACTUAL} KB"
 echo "Minimum size: 200 KB"
