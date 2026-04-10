cb0575237cedd6399c1f43dfd5fc108b81bb3b47
Eric Larson <larson.eric.d@gmail.com>
2026-04-09 20:25:10 -0400

    FIX: Version

--- assets/post_install_windows.bat
+++ assets/post_install_windows.bat
@@ -9,5 +9,8 @@ echo Disabling mamba package manager banner.
 echo Pinning BLAS implementation to OpenBLAS.
 echo libblas=*=*openblas >> "%PREFIX%\conda-meta\pinned"
 
+echo Setting read/write permissions on the Python installation to %USERNAME% (this can take a few minutes!).
+icacls %PREFIX% /grant %USERNAME%:(OI)(CI)M /c /t /q
+
 echo Running mne sys_info.
 "%PREFIX%\Scripts\conda" run mne sys_info -a || echo
