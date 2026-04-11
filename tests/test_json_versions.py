2e570135d439dc06a811690c85bfc918c4fd65c9
Eric Larson <larson.eric.d@gmail.com>
2026-04-10 21:11:04 -0400

    FIX: Packages

--- tests/test_json_versions.py
+++ tests/test_json_versions.py
@@ -32,23 +32,25 @@ construct_yaml_path = recipe_dir / "construct.yaml"
 params = yaml.safe_load(construct_yaml_path.read_text(encoding="utf-8"))
 installer_version = params["version"]
 specs = params["specs"]
+construct_lines = construct_yaml_path.read_text("utf-8").splitlines()
 del params
 
 # Extract versions from construct.yaml
 want_versions = {}
 for spec in specs:
-    if " =" not in spec or spec.count("=") < 2:
+    if not spec.count("="):
         continue
     package_name, package_version_and_build = spec.split(" ")
-    package_version = package_version_and_build.split("=")[1].rstrip("*")
-    package_build = package_version_and_build.split("=")[-1]
+    spec_parts = package_version_and_build.split("=")[1:]
+    package_version = spec_parts[0].rstrip("*")
+    package_build = spec_parts[1] if len(spec_parts) > 1 else None
     want_versions[package_name] = {
         "version": package_version,
         "build_string": package_build,
     }
 # for name in ("mne", "mne-installer-menu"):  # the most important ones!
 #     assert name in want_versions, f"{name} missing from construct.yml versions"
-assert len(want_versions) > 20, len(want_versions)  # lots of packages
+assert len(want_versions) > 50, want_versions  # lots of packages with version specs
 
 # Extract versions from created environment
 fname = dir_ / f"MNE-Python-{installer_version}-{sys_name}{sys_ext}.env.json"
@@ -61,9 +63,22 @@ for package in env_json:
         "build_string": str(package["build_string"]),
     }
 
+start = construct_lines.index("  # OS-specific")
+stop = construct_lines.index("  # End OS-specific defs")
+ignores = [line.strip() for line in construct_lines[start:stop]]
+ignores = [
+    line.split("#")[0].split("=")[0][2:].strip()
+    for line in ignores
+    if not line.startswith("#")
+]
+ignores += ["numpy", "numba"]
 # check versions
 for package_name, want in want_versions.items():
+    if package_name in ignores:
+        continue
+    assert package_name in got_versions, f"{package_name} missing from env.json"
     got = got_versions[package_name]
     msg = f"{package_name}: got {repr(got)} != want {repr(want)}"
     assert got["version"] == want["version"], msg
-    assert fnmatch.fnmatch(got["build_string"], want["build_string"]), msg
+    if want["build_string"] is not None:
+        assert fnmatch.fnmatch(got["build_string"], want["build_string"]), msg
