c99834ca4286a8ae2ce21b383fb58977ac409b56
Eric Larson <larson.eric.d@gmail.com>
2026-04-21 13:38:01 -0400

    FIX: Regex

--- tools/check_installation.sh
+++ tools/check_installation.sh
@@ -23,7 +23,7 @@ echo "::endgroup::"
 N=100
 echo "::group::package sizes (top $N)"
 # https://stackoverflow.com/a/67976448/2175965
-grep '"size":' ${CONDA_PREFIX}/conda-meta/*.json | sort -k3rn | sed 's/.*conda-meta\///g' | sed 's/"size": //g' | sed 's/,$//' > package_sizes.txt
+grep '"size":' ${CONDA_PREFIX}/conda-meta/*.json | sort -k3rn | sed -E 's/.*conda-meta\/(.+)\.json:\s+"size": (.+),/\1 \2/g' > package_sizes.txt
 head -n $N package_sizes.txt | column -t
 echo "::endgroup::"
 
